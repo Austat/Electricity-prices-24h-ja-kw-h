422324ecf7b156afca374ffeb3fcfc874f864401
Austat <250345494+Austat@users.noreply.github.com>
2026-06-12 14:04:25 +0300

    Update Start_Monitor.cmd

--- Start_Monitor.cmd
+++ Start_Monitor.cmd
@@ -1,2 +1,2 @@
 cd /d "%~dp0"
-C:\Users\janne\AppData\Local\Programs\Python\Python312\python.exe "E:\Python skriptit\sahkohinnat_gui_laajennettu_v4.py"
+C:\Users\<user>\AppData\Local\Programs\Python\Python312\python.exe "E:\Python skriptit\sahkohinnat_gui_laajennettu_v4.py"
